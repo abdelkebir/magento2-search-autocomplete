8b59c96dfec730569b70dadf475a9a9da0a2d04c
EL Harfali <elharfali.abdelkebir@gmail.com>
2018-11-08 01:09:30 +0100

    Some improvements

--- Block/SearchAutocomplete.php
+++ Block/SearchAutocomplete.php
@@ -4,45 +4,42 @@ class SearchAutocomplete extends \Magento\Framework\View\Element\Template
 {
     protected $_categoryCollectionFactory;
     protected $_storeManager;
-    
+    protected $_store;
+    protected $_categoryRepository;
         
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,        
         \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Store\Model\Store $store,
+        \Magento\Catalog\Model\CategoryRepository $categoryRepository,
         array $data = []
     )
     {
         $this->_categoryCollectionFactory 	=	$categoryCollectionFactory;
         $this->_storeManager 	=	$storeManager;
+        $this->_store = $store;
+        $this->_categoryRepository = $categoryRepository;
         parent::__construct($context, $data);
     }
     
     public function getCategoryCollection($isActive = true, $level = false, $sortBy = false, $pageSize = false)
     {
         $collection = $this->_categoryCollectionFactory->create();
-        $collection->addAttributeToSelect('*');        
-        
-        // select only active categories
+        $collection->addAttributeToSelect('*');
         if ($isActive) {
             $collection->addIsActiveFilter();
         }
-                
-        // select categories of certain level
         if ($level) {
             $collection->addLevelFilter($level);
         }
-        
-        // sort categories by some value
         if ($sortBy) {
             $collection->addOrderField($sortBy);
         }
-        
-        // select certain number of categories
         if ($pageSize) {
             $collection->setPageSize($pageSize); 
-        }    
-        
+        }
+        $collection->setOrder('position', 'ASC');
         return $collection;
     }
     public function getAjaxUrl(){
@@ -51,4 +48,11 @@ class SearchAutocomplete extends \Magento\Framework\View\Element\Template
 	public function getMediaUrl(){
 		return $this ->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA );
 	}
+    public function getRootCategoryId()
+    {
+        return $this->_store->getStoreRootCategoryId();
+    }
+    public function getCategories(){
+        return $this->_categoryRepository->get(2)->getChildrenCategories();
+    }
 }
